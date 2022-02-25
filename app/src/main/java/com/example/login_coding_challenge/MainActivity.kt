247113abd6e27e9e66806b3b838a06e61e62bbe6
Jeff <47189403+hastangbugoya@users.noreply.github.com>
2022-02-25 13:08:25 -0600

    first

--- app/src/main/java/com/example/login_coding_challenge/MainActivity.kt
+++ app/src/main/java/com/example/login_coding_challenge/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.login_coding_challenge
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,8 +13,11 @@ import androidx.appcompat.widget.AppCompatCheckBox
 import com.example.login_coding_challenge.data.LoginLocalData
 import com.example.login_coding_challenge.data.LoginRemoteData
 import com.example.login_coding_challenge.data.LoginService
+import com.example.login_coding_challenge.model.LoginUserRequest
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
@@ -21,7 +26,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, CompoundButton.O
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        GlobalScope.launch {
+            val response = loginService.loginUser(LoginUserRequest(emailTextInputEditText.getText().toString(), passwordTextInputEditText.getText().toString(),false))
+            if (response.statusCode == "0000") {
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
     private lateinit var loader: ProgressBar
@@ -35,12 +46,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, CompoundButton.O
 
     private val loginService = LoginService()
     private val loginRemoteData = LoginRemoteData(loginService)
-    private val loginLocalData = LoginLocalData(getSharedPreferences("com.example.login", MODE_PRIVATE))
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val loginLocalData = LoginLocalData(getSharedPreferences("com.example.login", MODE_PRIVATE))
         loader = findViewById(R.id.loader)
         loginTextView = findViewById(R.id.loginTextView)
         emailTextInputLayout = findViewById(R.id.emailTextInputLayout)
